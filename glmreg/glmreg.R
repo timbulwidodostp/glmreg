eef1df3850793b146998912037e9e19fdedae321
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2024-08-21 10:50:11 +0700

    Use glmreg With (In) R Software

--- glmreg/glmreg.R
+++ glmreg/glmreg.R
@@ -3,6 +3,7 @@
 # IG : @olahdatasemarang_
 # Fit a GLM with lasso (or elastic net), snet or mnet regularization Use glmreg With (In) R Software
 install.packages("mpath")
+install.packages("pamr")
 library("mpath")
 glmreg = read.csv("https://raw.githubusercontent.com/timbulwidodostp/glmreg/main/glmreg/glmreg.csv",sep = ";")
 # Estimation Fit a GLM with lasso (or elastic net), snet or mnet regularization Use glmreg With (In) R Software
